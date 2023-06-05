bb56a9c7581768b2beaeacd18b76860249cd77e7
Subham Gourisaria <79411176+SubhamBnk@users.noreply.github.com>
2023-06-05 15:47:41 +0530

    Update Firebase.js

--- Firebase/Firebase.js
+++ Firebase/Firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+   apiKey: "AIzaSyCp69ojxe9_6FtTmzYkb6DI84Fp_tk3RcU",
+  authDomain: "nth-suprstate-321819.firebaseapp.com",
+  projectId: "nth-suprstate-321819",
+  storageBucket: "nth-suprstate-321819.appspot.com",
+  messagingSenderId: "922064871392",
+  appId: "1:922064871392:web:884ffbebb8af99641b9585",
+  measurementId: "G-25YX1ZNBJ8"
 };
 const app = initializeApp(firebaseConfig);
 
